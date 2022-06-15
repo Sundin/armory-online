e525e2cb7849d1c81a79288c9446f401024e6103
Gustav Sundin <sundin666@gmail.com>
2022-06-15 09:39:27 +0200

    Simple voting function

--- src/pages/Vote.vue
+++ src/pages/Vote.vue
@@ -1,12 +1,23 @@
 <template>
   <main-layout>
+    <form @submit.prevent="submit">
+      <div v-for="song in songs" v-bind:key="song.songId">
+        <div class="form-group form-check">
+          <input type="checkbox" v-model="song.selected" id="accept" class="form-check-input" />
+          <label class="form-check-label" for="accept">{{ song.songTitle }}</label>
+        </div>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+
     <h3>Current Setlist:</h3>
     <div class="votes">
       <div v-for="item in sortedVotes" v-bind:key="item.songId">
-        <p>{{item.songTitle + " (" + item.count + " " + (item.count > 1 ? "votes" : "vote")}})</p>
+        <p>{{ item.songTitle + " (" + item.count + " " + (item.count > 1 ? "votes" : "vote") }})</p>
       </div>
     </div>
-
   </main-layout>
 </template>
 
@@ -24,6 +35,18 @@ export default {
   data() {
     return {
       votes: [],
+      songs: [
+        {
+          songId: '1x',
+          songTitle: 'Cool 1 x',
+          selected: false,
+        },
+        {
+          songId: '2x',
+          songTitle: 'Cool 2 x',
+          selected: false,
+        },
+      ],
     };
   },
   created() {
@@ -60,6 +83,26 @@ export default {
   },
   methods: {
     submit() {
+      const timestamp = Date.now();
+      const myVotes = this.songs
+        .filter((song) => song.selected)
+        .map((song) => ({
+          songId: song.songId,
+          songTitle: song.songTitle,
+          createdAt: timestamp,
+          email: 'hej2',
+        }));
+
+      myVotes.forEach((vote) => {
+        axios
+          .post(BASE_URL, vote)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
     },
   },
 };
