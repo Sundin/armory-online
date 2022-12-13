83e9920c501066e2146f469138365f85eeb27ea7
Gustav Sundin <sundin666@gmail.com>
2022-12-13 10:03:45 +0100

    Update press kit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,30 +5,25 @@
     <p>Contact: armoryofficial@gmail.com</p>
 
     <p>
-      Armory started out in 2012 and has been delivering hyper speed metal
-      since. After two demos, the debut full length album
+      Armory started out in 2012 and has been delivering hyper speed metal since. After two demos,
+      the debut full length album
       <span class="title">World Peace… Cosmic War</span>
-      was released via High Roller Records in 2016. The album got great reviews
-      from all around the world and was priced “Album of the year” by Metal
-      Invader. The band then got to share the stage with bigger bands such as
-      Pentagram and Nifelheim. With all the new experience the band went into
-      the studio again in late 2017 to record their second album.
-      <span class="title">The Search</span> was released in July 2018 via High
-      Roller and again the album got great reviews and it was proved that the
-      songwriting had developed in the right direction. The band was also able
-      to break new ground outside of Sweden with gigs in Germany, Belgium and
-      Denmark. Ratings from live reviews started to get higher and fact is that
-      Armory was the only band that got 5/5 on the Metal Magic Festival by the
-      zine Blastbeast.dk. With two albums done, Armory was finally ready to make
-      something that they had planned for years but previously didn't feel they
-      were ready for: a full blown sci-fi concept album with a story from start
-      to finish. This didn't just put more pressure on the lyrics but also to
-      make the music more dramatic to fit the story. The album takes the
-      listener through a dramatic journey aboard the starship
-      <span class="title">Mercurion</span>, named after the god Mercurius – the
-      winged messenger of the gods, protector of travelers and god of boundaries
-      between worlds. Mercurion was released in 2022 by the mighty Dying Victims
-      Productions.
+      was released via High Roller Records in 2016. The album got great reviews from all around the
+      world and was priced “Album of the year” by Metal Invader. The band then got to share the
+      stage with bigger bands such as Pentagram and Nifelheim. With all the new experience the band
+      went into the studio again in late 2017 to record their second album.
+      <span class="title">The Search</span> was released in July 2018 via High Roller and again the
+      album got great reviews and it was proved that the songwriting had developed in the right
+      direction. The band was also able to break new ground outside of Sweden with gigs in Germany,
+      Belgium and Denmark. Ratings from live reviews started to get higher and fact is that Armory
+      was the only band that got 5/5 on the Metal Magic Festival by the zine Blastbeast.dk. With two
+      albums done, Armory was finally ready to make something that they had planned for years but
+      previously didn't feel they were ready for: a full blown sci-fi concept album with a story
+      from start to finish. This didn't just put more pressure on the lyrics but also to make the
+      music more dramatic to fit the story. The album takes the listener through a dramatic journey
+      aboard the starship <span class="title">Mercurion</span>, named after the god Mercurius – the
+      winged messenger of the gods, protector of travelers and god of boundaries between worlds.
+      Mercurion was released in 2022 by the mighty Dying Victims Productions.
     </p>
 
     <p>
@@ -44,7 +39,6 @@
 
     <img src="../assets/band_photo.jpg" />
     <h1>Discography</h1>
-    <p><b>2022: Mercurion</b></p>
     <p>2013: Open Fire! (Demo)</p>
     <p>2015: S.M.I. (Demo)</p>
     <p>
@@ -53,47 +47,42 @@
     </p>
     <p><b>2016: World Peace... Cosmic War</b></p>
     <p><b>2018: The Search</b></p>
-    <p>2018: Live in the Abyss (Live Cassette)</p>
-    <p><b>2021 (upcoming): Mercurion</b></p>
+    <p>2018: Live in the Abyss (<a href="/fanclub">Fanclub</a>-only Live Cassette)</p>
+    <p><b>2022: Mercurion</b></p>
 
     <h1>Links</h1>
     <p>
       <a href="https://armory417.bandcamp.com/">Bandcamp</a>
     </p>
     <p>
-      <a
-        href="https://open.spotify.com/artist/2mAu2bKoNDxD1jybBS1rku?si=PdF2nwUIRpam5CRc7V3i9Q"
+      <a href="https://open.spotify.com/artist/2mAu2bKoNDxD1jybBS1rku?si=PdF2nwUIRpam5CRc7V3i9Q"
         >Spotify</a
       >
     </p>
     <p>
-      <a href="https://youtu.be/UYj3EnEON8k">Vault Seven official video</a>
-    </p>
-    <p>
-      <a href="https://www.youtube.com/watch?v=hTNFsTV38hA&t=80s"
-        >Hyperion lyric video</a
-      >
-    </p>
-    <p>
-      <a href="https://www.youtube.com/watch?v=9YDNAMrtTVU"
-        >The Search album trailer</a
-      >
-    </p>
-    <p>
-      <a href="https://www.youtube.com/user/ArmoryOfficial"
-        >Official YouTube channel</a
-      >
-    </p>
-    <p>
-      <a href="https://www.metal-archives.com/bands/Armory/3540372230"
-        >Metal Archives</a
-      >
+      <a href="https://www.youtube.com/user/ArmoryOfficial">Official YouTube channel</a>
     </p>
     <p>
       <a href="https://www.facebook.com/armoryofficial/">Facebook</a>
     </p>
     <p>
       <a href="https://www.instagram.com/armorysweden/">Instagram</a>
+    </p>
+    <p>
+      <a href="https://www.jawbreaker.se/product-tag/armory/">Official Merch</a>
+    </p>
+    <h2>Videos</h2>
+    <p>
+      <a href="https://www.youtube.com/watch?v=Uw76Hz1XCz8">Wormhole Escape music video</a>
+    </p>
+    <p>
+      <a href="https://youtu.be/UYj3EnEON8k">Vault Seven music video</a>
+    </p>
+    <p>
+      <a href="https://www.youtube.com/watch?v=hTNFsTV38hA&t=80s">Hyperion lyric video</a>
+    </p>
+    <p>
+      <a href="https://www.youtube.com/watch?v=9YDNAMrtTVU">The Search album trailer</a>
     </p>
   </main-layout>
 </template>
