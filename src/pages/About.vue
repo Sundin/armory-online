c9514d6face272b71c7c36f120a5485a77b542ac
Gustav Sundin <sundin666@gmail.com>
2024-12-27 17:20:36 +0100

    Updated press kit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,35 +1,34 @@
 <template>
   <main-layout>
     <h1>About Armory</h1>
-    <p>Hyper Speed Metal from Hisingen Island in Gothenburg, Sweden.</p>
+    <p>Cosmic Speed Metal from Gothenburg, Sweden (Earth).</p>
     <p>Contact: armoryofficial@gmail.com</p>
 
     <p>
-      Armory started out in 2012 and has been delivering hyper speed metal since. After two demos,
-      the debut full length album
+      Out of the deep void in the far corners of the universe, the entity known as Armory was born
+      in the year 2012 C.E. With the guiding light of the fiery stars Judas Priest, Agent Steel and
+      Slayer, Armory set out on a cosmic path delivering hyperspeed metal to every planet, system,
+      and galaxy in their path. The two first albums <span class="title">World Peace… Cosmic War
+
+      </span>
+      (2016) and <span class="title">The Search</span>
+      (2018) was released via High Roller Records. Both got great reviews and
       <span class="title">World Peace… Cosmic War</span>
-      was released via High Roller Records in 2016. The album got great reviews from all around the
-      world and was priced “Album of the year” by Metal Invader. The band then got to share the
-      stage with bigger bands such as Pentagram and Nifelheim. With all the new experience the band
-      went into the studio again in late 2017 to record their second album.
-      <span class="title">The Search</span> was released in July 2018 via High Roller and again the
-      album got great reviews and it was proved that the songwriting had developed in the right
-      direction. The band was also able to break new ground outside of Sweden with gigs in Germany,
-      Belgium and Denmark. Ratings from live reviews started to get higher and fact is that Armory
-      was the only band that got 5/5 on the Metal Magic Festival by the zine Blastbeast.dk. With two
-      albums done, Armory was finally ready to make something that they had planned for years but
-      previously didn't feel they were ready for: a full blown sci-fi concept album with a story
-      from start to finish. This didn't just put more pressure on the lyrics but also to make the
-      music more dramatic to fit the story. The resulting album takes the listener through a
-      dramatic journey aboard the starship <span class="title">Mercurion</span>, named after the god
-      Mercurius – the winged messenger of the gods, protector of travelers and god of boundaries
-      between worlds. <span class="title">Mercurion</span> was released in 2022 by the mighty Dying
-      Victims Productions.
+      was priced “Album of the year” by Metal Invader. Under the new flag of Dying Victims
+      Productions the third album <span class="title">Mercurion</span> (2022)
+      was unleashed followed by a European tour in
+      2023. This further cemented that Armory is a force to be reckoned with on the intergalactic
+      battleground. Armorys upcoming operations are set in motion and the next chapter starts with
+      the deployment of the single Black Star in February 2025 C.E. Expect a rapid barrage of
+      hyperspeed metal with influences from the dark cold void that engulfs us all.
     </p>
 
     <p>
       <a href="/press_kit.zip" download>Download press kit</a>
     </p>
+
+    <img src="../assets/armory2024.png" />
+
     <h1>Lineup</h1>
 
     <p>Anglegrinder – Thunder Bass</p>
@@ -38,7 +37,6 @@
     <p>G.G. Sundin – Guitars</p>
     <p>Ingelman – Guitars</p>
 
-    <img src="../assets/band_photo.jpg" />
     <h1>Album Discography</h1>
     <p><b>2016: World Peace... Cosmic War</b></p>
     <p><b>2018: The Search</b></p>
@@ -52,6 +50,7 @@
       <span class="title">High Speed Death</span>)
     </p>
     <p>2018: Live in the Abyss (<a href="/fanclub">Fanclub</a>-only Live Cassette)</p>
+    <p>2025: Black Star (7" single)</p>
 
     <h1>Links</h1>
     <p>
