4f2250d2bb2e87db17ce484c6922865f367bcf03
Gustav Sundin <sundin666@gmail.com>
2024-12-27 17:05:32 +0100

    Fanclub disbanded

--- src/fanclub/Fanclub.vue
+++ src/fanclub/Fanclub.vue
@@ -1,28 +1,18 @@
 <template>
   <main-layout>
-    <h1>Join the Space Marauders!</h1>
-    <p>
-      The Space Marauders is the official Armory fanclub.
-      Membership costs 150 SEK per year and includes an exclusive gift.
-      The gift for previous years' fanclub editions have been rare merchandise
-      such as special edition t-shirt and patches,
-      as well as the highly sought-after "Live in the Abyss" live cassette.
-      We have members from all around the galaxy.
-    </p>
-    <h3>
-      <a
-        href="https://www.jawbreaker.se/product/space-marauders-membership/"
-      >Click here to join the fanclub!</a>
-    </h3>
+    <h1>Space Marauders FC</h1>
 
     <p>
-      Already a member? <a href="/vault7">Click here to enter the restricted fanclub area.</a>
+      <a href="/vault7">Click here to enter the restricted fanclub area.</a>
     </p>
+    <p>The Space Marauders was the now disbanded Armory fanclub.</p>
 
     <p>
-      In this image you can see some of the gifts we have sent out
-      to our faithful fanclub members during the past years:
-      </p>
+      Each year a unique gift was distributed to all the fanclub members, such as special edition
+      t-shirt and patches, and of course the now highly sought-after "Live in the Abyss" live
+      cassette. In this image you can see some of the gifts we have sent out to our faithful fanclub
+      members over the years:
+    </p>
     <a href="https://www.jawbreaker.se/product/space-marauders-membership/">
       <img src="../assets/fanclub_merch.jpg" />
     </a>
